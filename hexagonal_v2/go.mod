c254226e1f618a8f3af6d043ad391b20783f6e34
Force <naphatnaphat85@gmail.com>
2024-07-12 14:08:57 +0700

    edit dockerfile, init ports

---
+++ hexagonal_v2/go.mod
@@ -0,0 +1,3 @@
+module hexagonal_v2
+
+go 1.20
--- hexagonal_v2/internal/core/domain/domain.go
+++ hexagonal_v2/internal/core/domain/domain.go
@@ -1,12 +1,23 @@
 package domain
 
 type User struct {
-	ID       int
-	Username string
+	ID       int       `json:"id"`
+	Username string    `json:"username"`
+	Airpod   []*Airpod `json:"airpod"`
 }
 
 type Airpod struct {
-	ID    int
-	Name  string
-	Model string
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	Model      string    `json:"model"`
+	UserID     int       `json:"user_id"`
+	User       *User     `json:"user"`
+	LocationID int       `json:"location_id"`
+	Location   *Location `json:"location"`
+}
+
+type Location struct {
+	ID int `json:"id"`
+	X  int `json:"x"`
+	Y  int `json:"y"`
 }

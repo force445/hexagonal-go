57be68f8c8c7fc5865b5f787d8128d6f1324ef89
Force <naphatnaphat85@gmail.com>
2024-08-05 10:32:31 +0700

    add deploy normal and helmchart

--- hexagonal_v2/runserver.sh
+++ hexagonal_v2/runserver.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+#load config from .env
+export $(cat .env | xargs)
+
+echo "DB_HOST: ${DB_HOST}"
+echo "DB_PORT: ${DB_PORT}"
+
 while ! nc -z ${DB_HOST} ${DB_PORT}; do
     echo "Waiting for the Postgres database to start...";
     sleep 1;
 done;
 
-# Run the Go application directly
-cd cmd/http/
-dogo -c dogo.json
+# Run the Go FOR DEVELOPMENT ONLY
+# cd cmd/http/
+# dogo -c dogo.json
+
+exec /app/bin/main
